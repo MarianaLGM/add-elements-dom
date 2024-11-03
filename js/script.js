baaa60aa5c176e07c715e020aabac5e3573a35af
MarianaLGM <mariana_lobeto@hotmail.com>
2024-11-03 23:35:11 +0100

    add index.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
  // Aquí tu código
+/*esto es lo que hace funcionar el prompt de manera correcta:*/
+const lista=document.getElementById ("lista");
+const agregar=document.getElementById ("agregar");
+
+
+agregar.addEventListener ("click", function(){
+    playa  = prompt("¿Cuál es tu playa favorita?",""),
+    li.innerHTML = (" Mi playa favorita es " + playa);
+});
+
+const ul=document.createElement ("ul");
+const li=document.createElement ("li");
+ul.append(li);
+document.body.append(ul);
+
+
+/*const elementAgregar = document.createElement ("agregar");
+const elementLista = document.createElement ("lista");
+const elementPlaya = document.createElement("playa");
+const elementAgregar = document.createElement ("agregar");
+const elementPlaya = document.createElement("playa");
+elementLista.appendChild (agregar);
+*/
+
+
+/*una forma de hacerlo pero sin pulsar el boton
+let ul = document.createElement('ul');
+document.body.append(ul);
+
+while (true) {
+  let data = prompt("Ingresa el texto para el ítem de la lista", "");
+
+  if (!data) {
+    break;
+  }
+
+  let li = document.createElement('li');
+  li.textContent = data;
+  ul.append(li);
+
+}*/
+
+
